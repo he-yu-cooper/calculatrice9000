aaa7975fadcb6f6042253fe16ec2364e50b9ba89
he-yu-cooper <he.yu-cooper@laplateforme.io>
2023-11-28 09:31:52 +0100

    calculatrice

---
+++ calculate.py
@@ -0,0 +1,24 @@
+def get_input():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operator = input("Enter the operator (+, -, *, /): ")
+    return num1, num2, operator
+
+def calculate(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operator"
+
+num1, num2, operator = get_input()
+result = calculate(num1, num2, operator)
+print("The result is: ", result)
